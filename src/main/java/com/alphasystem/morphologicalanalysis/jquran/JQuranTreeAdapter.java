da917842581262d2086a802ab73f993fd8489f84
sali <sali2@loyalty.com>
2015-06-23 12:12:29 -0400

    Added support for add empty node and make relationship

--- src/main/java/com/alphasystem/morphologicalanalysis/jquran/JQuranTreeAdapter.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/jquran/JQuranTreeAdapter.java
@@ -11,8 +11,8 @@ import com.alphasystem.arabic.model.ArabicWord;
 import com.alphasystem.tanzil.TanzilTool;
 import com.alphasystem.tanzil.model.Verse;
 
-import static com.alphasystem.arabic.model.ArabicLetterType.CLOSE_BRACKET;
-import static com.alphasystem.arabic.model.ArabicLetterType.OPEN_BRACKET;
+import static com.alphasystem.arabic.model.ArabicLetterType.ORNATE_LEFT_PARENTHESIS;
+import static com.alphasystem.arabic.model.ArabicLetterType.ORNATE_RIGHT_PARENTHESIS;
 import static com.alphasystem.arabic.model.ArabicWord.*;
 
 /**
@@ -25,8 +25,8 @@ public class JQuranTreeAdapter {
 
 	public static ArabicWord getVerse(int chapterNumber, int verseNumber) {
 		Verse verse = tanzilTool.getVerse(chapterNumber, verseNumber);
-		ArabicWord verseNumberWord = concatenate(getWord(CLOSE_BRACKET),
-				getArabicNumber(verseNumber), getWord(OPEN_BRACKET));
+		ArabicWord verseNumberWord = concatenate(getWord(ORNATE_RIGHT_PARENTHESIS),
+				getArabicNumber(verseNumber), getWord(ORNATE_LEFT_PARENTHESIS));
 		return concatenateWithSpace(verse.getVerse(), verseNumberWord);
 	}
 
--- src/test/java/com/alphasystem/morphologicalanalysis/repository/test/BulkDataCreation.java
+++ src/test/java/com/alphasystem/morphologicalanalysis/repository/test/BulkDataCreation.java
@@ -8,14 +8,19 @@ import com.alphasystem.morphologicalanalysis.spring.support.MongoConfig;
 import com.alphasystem.morphologicalanalysis.spring.support.MorphologicalAnalysisSpringConfiguration;
 import com.alphasystem.morphologicalanalysis.spring.support.WordByWordConfig;
 import com.alphasystem.morphologicalanalysis.util.MorphologicalAnalysisRepositoryUtil;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
+import static com.alphasystem.morphologicalanalysis.wordbyword.model.support.PartOfSpeech.NOUN;
+import static com.alphasystem.morphologicalanalysis.wordbyword.model.support.PartOfSpeech.VERB;
 import static com.alphasystem.util.Utils.getTimeConsumed;
 import static java.lang.System.currentTimeMillis;
 import static java.lang.System.out;
+import static java.util.Collections.singletonList;
 
 /**
  * @author sali
@@ -45,6 +50,33 @@ public class BulkDataCreation extends AbstractTestNGSpringContextTests {
         out.printf("Total time consume is {%s}", getTimeConsumed(endTime
                 - startTime));
         out.println();
+    }
+
+    @Test(dependsOnMethods = {"createChapters"})
+    public void createEmptyTokens() {
+        Token token = new Token().withChapterNumber(0).withVerseNumber(1).withTokenNumber(1).withToken("(*)")
+                .withHidden(true);
+
+        Location location = new Location().withChapterNumber(token.getChapterNumber())
+                .withVerseNumber(token.getVerseNumber()).withTokenNumber(token.getTokenNumber())
+                .withLocationIndex(1).withPartOfSpeech(NOUN).withStartIndex(0)
+                .withEndIndex(token.getTokenWord().getLength());
+
+        token.setLocations(singletonList(location));
+
+        repositoryUtil.getTokenRepository().save(token);
+
+        token = new Token().withChapterNumber(0).withVerseNumber(1).withTokenNumber(2).withToken("(*)")
+                .withHidden(true);
+
+        location = new Location().withChapterNumber(token.getChapterNumber())
+                .withVerseNumber(token.getVerseNumber()).withTokenNumber(token.getTokenNumber())
+                .withLocationIndex(1).withPartOfSpeech(VERB).withStartIndex(0)
+                .withEndIndex(token.getTokenWord().getLength());
+
+        token.setLocations(singletonList(location));
+
+        repositoryUtil.getTokenRepository().save(token);
     }
 
     public MorphologicalAnalysisRepositoryUtil getRepositoryUtil() {
