a47b22b32cc21cf98b5923ba6947914a4d212ece
sali <sali2@loyalty.com>
2015-06-30 09:24:56 -0400

    correct package name

--- src/main/java/com/alphasystem/morphologicalanalysis/spring/support/MorphologicalAnalysisSpringConfiguration.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/spring/support/MorphologicalAnalysisSpringConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.alphasystem.persistence.mongo.spring.support",
 		"com.alphasystem.morphologicalanalysis.jquran",
 		"com.alphasystem.morphologicalanalysis.util",
-		"com.alphasystem.morphologicalanalysis.wordbyword.repository.listener",
+		"com.alphasystem.morphologicalanalysis.wordbyword.listener",
 		"com.alphasystem.morphologicalanalysis.graph.listener"})
 public class MorphologicalAnalysisSpringConfiguration {
 
