f2884d7bc0b68147efc8fb5c81f72834b4694a42
sali <sali2@loyalty.com>
2015-08-07 09:33:08 -0400

    Hidden node support

---
+++ src/test/java/com/alphasystem/morphologicalanalysis/repository/test/DeleteData.java
@@ -0,0 +1,81 @@
+package com.alphasystem.morphologicalanalysis.repository.test;
+
+import com.alphasystem.morphologicalanalysis.graph.model.DependencyGraph;
+import com.alphasystem.morphologicalanalysis.graph.model.Fragment;
+import com.alphasystem.morphologicalanalysis.graph.model.Relationship;
+import com.alphasystem.morphologicalanalysis.graph.model.Terminal;
+import com.alphasystem.morphologicalanalysis.graph.model.support.TerminalType;
+import com.alphasystem.morphologicalanalysis.graph.repository.DependencyGraphRepository;
+import com.alphasystem.morphologicalanalysis.graph.repository.FragmentRepository;
+import com.alphasystem.morphologicalanalysis.graph.repository.RelationshipRepository;
+import com.alphasystem.morphologicalanalysis.graph.repository.TerminalRepository;
+import com.alphasystem.morphologicalanalysis.spring.support.GraphConfig;
+import com.alphasystem.morphologicalanalysis.spring.support.MongoConfig;
+import com.alphasystem.morphologicalanalysis.spring.support.MorphologicalAnalysisSpringConfiguration;
+import com.alphasystem.morphologicalanalysis.spring.support.WordByWordConfig;
+import com.alphasystem.morphologicalanalysis.util.MorphologicalAnalysisRepositoryUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.alphasystem.morphologicalanalysis.graph.model.support.TerminalType.EMPTY;
+import static com.alphasystem.morphologicalanalysis.graph.model.support.TerminalType.HIDDEN;
+import static java.lang.String.format;
+
+/**
+ * @author sali
+ */
+@ContextConfiguration(classes = {MongoConfig.class, WordByWordConfig.class, GraphConfig.class,
+        MorphologicalAnalysisSpringConfiguration.class})
+public class DeleteData extends AbstractTestNGSpringContextTests {
+
+    private MorphologicalAnalysisRepositoryUtil repositoryUtil;
+
+    @Autowired
+    public void setRepositoryUtil(MorphologicalAnalysisRepositoryUtil repositoryUtil) {
+        this.repositoryUtil = repositoryUtil;
+    }
+
+    @Test
+    @Parameters({"id"})
+    public void deleteDependencyGraph(String id) {
+        DependencyGraphRepository dependencyGraphRepository = repositoryUtil.getDependencyGraphRepository();
+
+        DependencyGraph dependencyGraph = dependencyGraphRepository.findOne(id);
+        if (dependencyGraph == null) {
+            System.out.println(format("No DependencyGraph found with ID {%s}", id));
+            return;
+        }
+
+        TerminalRepository terminalRepository = repositoryUtil.getTerminalRepository();
+        List<Terminal> terminals = dependencyGraph.getTerminals();
+        for (Terminal terminal : terminals) {
+            if (terminal == null) {
+                continue;
+            }
+            TerminalType terminalType = terminal.getTerminalType();
+            if (terminalType.equals(EMPTY) || terminalType.equals(HIDDEN)) {
+                continue;
+            }
+            terminalRepository.delete(terminal);
+        }
+
+        RelationshipRepository relationshipRepository = repositoryUtil.getRelationshipRepository();
+        List<Relationship> relationships = dependencyGraph.getRelationships();
+        for (Relationship relationship : relationships) {
+            relationshipRepository.delete(relationship);
+        }
+
+        FragmentRepository fragmentRepository = repositoryUtil.getFragmentRepository();
+        List<Fragment> fragments = dependencyGraph.getFragments();
+        for (Fragment fragment : fragments) {
+            fragmentRepository.delete(fragment);
+        }
+
+        dependencyGraphRepository.delete(dependencyGraph);
+    }
+}
