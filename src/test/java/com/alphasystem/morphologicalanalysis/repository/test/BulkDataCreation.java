92d5a5d65f91e29507be754d3b47ae412125d0e4
sali <sali2@loyalty.com>
2015-11-07 07:39:03 -0500

    method move to separate class

--- src/test/java/com/alphasystem/morphologicalanalysis/repository/test/BulkDataCreation.java
+++ src/test/java/com/alphasystem/morphologicalanalysis/repository/test/BulkDataCreation.java
@@ -3,28 +3,19 @@
  */
 package com.alphasystem.morphologicalanalysis.repository.test;
 
-import com.alphasystem.arabic.model.ProNoun;
 import com.alphasystem.morphologicalanalysis.spring.support.GraphConfig;
 import com.alphasystem.morphologicalanalysis.spring.support.MongoConfig;
 import com.alphasystem.morphologicalanalysis.spring.support.MorphologicalAnalysisSpringConfiguration;
 import com.alphasystem.morphologicalanalysis.spring.support.WordByWordConfig;
 import com.alphasystem.morphologicalanalysis.util.MorphologicalAnalysisRepositoryUtil;
-import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
-import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.alphasystem.morphologicalanalysis.wordbyword.model.support.PartOfSpeech.*;
 import static com.alphasystem.util.Utils.getTimeConsumed;
-import static java.lang.String.format;
 import static java.lang.System.currentTimeMillis;
 import static java.lang.System.out;
-import static java.util.Collections.singletonList;
 
 /**
  * @author sali
@@ -56,49 +47,6 @@ public class BulkDataCreation extends AbstractTestNGSpringContextTests {
         out.println();
     }
 
-    @Test(dependsOnMethods = {"createChapters"})
-    public void createImpliedTokens() {
-        Token token = new Token().withChapterNumber(0).withVerseNumber(1).withTokenNumber(1).withToken("(*)")
-                .withHidden(true);
-
-        Location location = new Location().withChapterNumber(token.getChapterNumber())
-                .withVerseNumber(token.getVerseNumber()).withTokenNumber(token.getTokenNumber())
-                .withLocationIndex(1).withPartOfSpeech(NOUN).withStartIndex(0)
-                .withEndIndex(token.getTokenWord().getLength());
-        System.out.println(format("creating implied location: %s (POS: %s)", location, location.getPartOfSpeech()));
-
-        token.setLocations(singletonList(location));
-
-        System.out.println(format("Creating implied token: %s", token));
-        repositoryUtil.getTokenRepository().save(token);
-
-        token = new Token().withChapterNumber(0).withVerseNumber(1).withTokenNumber(2).withToken("(*)")
-                .withHidden(true);
-
-        location = new Location().withChapterNumber(token.getChapterNumber())
-                .withVerseNumber(token.getVerseNumber()).withTokenNumber(token.getTokenNumber())
-                .withLocationIndex(1).withPartOfSpeech(VERB).withStartIndex(0)
-                .withEndIndex(token.getTokenWord().getLength());
-        System.out.println(format("creating implied location: %s (POS: %s)", location, location.getPartOfSpeech()));
-
-        token.setLocations(singletonList(location));
-
-        System.out.println(format("Creating implied token: %s", token));
-        repositoryUtil.getTokenRepository().save(token);
-    }
-
-    @Test(dependsOnMethods = {"createImpliedTokens"})
-    public void createPronounTokens() {
-        List<Token> tokens = new ArrayList<>();
-        int verseNumber = 3;
-        int tokenNumber = 1;
-        for (ProNoun proNoun : ProNoun.values()) {
-            tokens.add(createToken(tokenNumber, verseNumber, proNoun));
-            tokenNumber++;
-        }
-        repositoryUtil.getTokenRepository().save(tokens);
-    }
-
     public MorphologicalAnalysisRepositoryUtil getRepositoryUtil() {
         return repositoryUtil;
     }
@@ -107,23 +55,6 @@ public class BulkDataCreation extends AbstractTestNGSpringContextTests {
     public void setRepositoryUtil(
             MorphologicalAnalysisRepositoryUtil repositoryUtil) {
         this.repositoryUtil = repositoryUtil;
-    }
-
-    private Token createToken(Integer tokenNumber, Integer verseNumber, ProNoun proNoun) {
-        Token token = new Token().withChapterNumber(0).withVerseNumber(verseNumber).withTokenNumber(tokenNumber)
-                .withHidden(true).withToken(format("(%s)", proNoun.getLabel().toUnicode()));
-        token.setId(proNoun.name());
-
-        Location location = new Location().withChapterNumber(token.getChapterNumber())
-                .withVerseNumber(token.getVerseNumber()).withTokenNumber(token.getTokenNumber())
-                .withLocationIndex(1).withPartOfSpeech(PRONOUN).withStartIndex(0)
-                .withEndIndex(token.getTokenWord().getLength());
-        System.out.println(format("creating hidden pronoun location: %s (POS: %s)", location,
-                location.getPartOfSpeech()));
-
-        token.setLocations(singletonList(location));
-
-        return token;
     }
 
 }
