7beb4baf215deda6ef8e92564c0ed397afc45e00
sali <sali2@loyalty.com>
2015-12-20 21:17:55 -0500

    checking for null root letters and/or form before saving instead of initDispaly

--- src/main/java/com/alphasystem/morphologicalanalysis/morphology/listener/MorphologicalEntryEventListener.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/morphology/listener/MorphologicalEntryEventListener.java
@@ -2,6 +2,7 @@ package com.alphasystem.morphologicalanalysis.morphology.listener;
 
 import com.alphasystem.morphologicalanalysis.morphology.model.MorphologicalEntry;
 import com.alphasystem.persistence.mongo.repository.DocumentEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,4 +10,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MorphologicalEntryEventListener extends DocumentEventListener<MorphologicalEntry> {
+
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<MorphologicalEntry> event) {
+        super.onBeforeConvert(event);
+        MorphologicalEntry source = event.getSource();
+        // if at this point either of root letters or form is null then raise the flag
+        if (source.isEmpty()) {
+            throw new RuntimeException("RootLetters and/or form cannot be null.");
+        }
+    }
 }
