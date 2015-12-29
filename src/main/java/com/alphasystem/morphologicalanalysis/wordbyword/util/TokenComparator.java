d582aae248c183decf126f964e1cdf17896c4373
sali <sali2@loyalty.com>
2015-12-29 11:54:20 -0500

    compare chanpter number as well

--- src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/util/TokenComparator.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/util/TokenComparator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.alphasystem.morphologicalanalysis.wordbyword.util;
 
@@ -9,28 +9,28 @@ import java.util.Comparator;
 
 /**
  * @author sali
- * 
  */
 public class TokenComparator implements Comparator<Token> {
 
-	@Override
-	public int compare(Token o1, Token o2) {
-		int result = 0;
-		if (o1 == null && o2 == null) {
-			result = 0;
-		} else if (o1 == null) {
-			result = -1;
-		} else if (o2 == null) {
-			result = 1;
-		} else {
-			result = o1.getVerseNumber().compareTo(o2.getVerseNumber());
-			if (result == 0) {
-				Integer tn1 = o1.getTokenNumber();
-				Integer tn2 = o2.getTokenNumber();
-				result = tn1.compareTo(tn2);
-			}
-		}
-		return result;
-	}
+    @Override
+    public int compare(Token o1, Token o2) {
+        int result = 0;
+        if (o1 == null && o2 == null) {
+            result = 0;
+        } else if (o1 == null) {
+            result = -1;
+        } else if (o2 == null) {
+            result = 1;
+        } else {
+            result = o1.getChapterNumber().compareTo(o2.getChapterNumber());
+            if (result == 0) {
+                result = o1.getVerseNumber().compareTo(o2.getVerseNumber());
+                if (result == 0) {
+                    result = o1.getTokenNumber().compareTo(o2.getTokenNumber());
+                }
+            }
+        }
+        return result;
+    }
 
 }
