12d4a9ff6d1eaf8d4b50706b967b49bc04be2311
sali <sali2@loyalty.com>
2016-02-04 08:57:13 -0500

    Changes to use GridFS

--- src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DefaultDictionaryNotesRepository.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DefaultDictionaryNotesRepository.java
@@ -1,5 +1,6 @@
 package com.alphasystem.morphologicalanalysis.morphology.repository;
 
+import com.alphasystem.morphologicalanalysis.morphology.model.DictionaryNotes;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
@@ -9,75 +10,47 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Path;
-
-import static com.alphasystem.util.AppUtil.fastCopy;
-import static java.lang.String.format;
-import static java.nio.file.Files.newInputStream;
-import static java.nio.file.Files.newOutputStream;
-import static java.nio.file.Paths.get;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.WRITE;
-
 /**
  * @author sali
  */
 @Service
 public class DefaultDictionaryNotesRepository implements DictionaryNotesRepository {
 
-    static {
-        if (!DEFAULT_NOTES_STORAGE.exists()) {
-            DEFAULT_NOTES_STORAGE.mkdirs();
-        }
-    }
-
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
-    private static Query getByFileNameQuery(RootLetters rootLetters) {
-        return new Query(Criteria.where("filename").is(getFileName(rootLetters)));
-    }
-
-    private static String getFileName(RootLetters rootLetters) {
-        return format("%s%s", rootLetters.getName(), DEFAULT_EXTENSION);
-    }
-
-    private static Path getFilePath(RootLetters rootLetters) {
-        return get(DEFAULT_NOTES_STORAGE.getPath(), getFileName(rootLetters));
+    private static Query getByFileNameQuery(DictionaryNotes dictionaryNotes) {
+        return new Query(Criteria.where("filename").is(dictionaryNotes.getFileName()));
     }
 
     @Override
-    public Path store(RootLetters rootLetters) throws IOException {
-        GridFSDBFile gridFSFile = gridFsTemplate.findOne(getByFileNameQuery(rootLetters));
+    public DictionaryNotes store(DictionaryNotes dictionaryNotes) {
+        GridFSDBFile gridFSFile = gridFsTemplate.findOne(getByFileNameQuery(dictionaryNotes));
         if (gridFSFile != null) {
             // file already exists, delete first
-            delete(rootLetters);
+            delete(dictionaryNotes);
         }
-        GridFSFile file;
-        final String fileName = getFileName(rootLetters);
-        final Path filePath = getFilePath(rootLetters);
-        try (InputStream inputStream = newInputStream(filePath)) {
-            file = gridFsTemplate.store(inputStream, fileName, "plain/text");
-        } catch (IOException e) {
-            throw e;
-        }
-        return filePath;
+        final GridFSFile file = gridFsTemplate.store(dictionaryNotes.getInputStream(), dictionaryNotes.getFileName(), "plain/text");
+        dictionaryNotes.setId(file.getId().toString());
+        return dictionaryNotes;
     }
 
     @Override
-    public Path retrieve(RootLetters rootLetters) throws IOException {
-        final GridFSDBFile file = gridFsTemplate.findOne(getByFileNameQuery(rootLetters));
-        final Path filePath = getFilePath(rootLetters);
-        final OutputStream outputStream = newOutputStream(filePath, WRITE, CREATE);
-        fastCopy(file.getInputStream(), outputStream);
-        return filePath;
+    public DictionaryNotes retrieve(RootLetters rootLetters) {
+        DictionaryNotes dictionaryNotes = new DictionaryNotes(rootLetters);
+        final GridFSDBFile file = gridFsTemplate.findOne(getByFileNameQuery(dictionaryNotes));
+        if (file == null) {
+            // no dictionary notes for these root letters
+            return dictionaryNotes;
+        }
+
+        dictionaryNotes.setId(file.getId().toString());
+        dictionaryNotes.setInputStream(file.getInputStream());
+        return dictionaryNotes;
     }
 
     @Override
-    public void delete(RootLetters rootLetters) {
-        gridFsTemplate.delete(getByFileNameQuery(rootLetters));
+    public void delete(DictionaryNotes dictionaryNotes) {
+        gridFsTemplate.delete(getByFileNameQuery(dictionaryNotes));
     }
 }
--- src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DictionaryNotesRepository.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DictionaryNotesRepository.java
@@ -1,40 +1,30 @@
 package com.alphasystem.morphologicalanalysis.morphology.repository;
 
+import com.alphasystem.morphologicalanalysis.morphology.model.DictionaryNotes;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-
-import static com.alphasystem.util.AppUtil.USER_HOME_DIR;
 
 /**
  * @author sali
  */
 public interface DictionaryNotesRepository {
 
-    String DEFAULT_EXTENSION = ".adoc";
-    File DEFAULT_FOLDER = new File(".wordbyword", "dictionary");
-    File DEFAULT_NOTES_STORAGE = new File(USER_HOME_DIR, DEFAULT_FOLDER.getPath());
-
     /**
      * Stores the dictionary notes related to given {@link RootLetters} into database. If the notes already exists
      * in the database then this method will replace them.
      *
-     * @param rootLetters given root letters
-     * @return String representing the file name of notes
-     * @throws IOException
+     * @param dictionaryNotes given root letters
+     * @return
      */
-    Path store(RootLetters rootLetters) throws IOException;
+    DictionaryNotes store(DictionaryNotes dictionaryNotes);
 
     /**
      * @param rootLetters given root letters
      * @return path representing the notes
      */
-    Path retrieve(RootLetters rootLetters) throws IOException;
+    DictionaryNotes retrieve(RootLetters rootLetters);
 
     /**
-     * @param rootLetters
+     * @param dictionaryNotes
      */
-    void delete(RootLetters rootLetters);
+    void delete(DictionaryNotes dictionaryNotes);
 }
