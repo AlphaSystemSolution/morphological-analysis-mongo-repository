61edd3b7ba567d29926c15e4e2b9db145efea8af
sali <sali2@loyalty.com>
2016-02-05 08:51:38 -0500

    adding metadata and changing query to use metadata instead of file name

--- src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DefaultDictionaryNotesRepository.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/morphology/repository/DefaultDictionaryNotesRepository.java
@@ -1,7 +1,10 @@
 package com.alphasystem.morphologicalanalysis.morphology.repository;
 
+import com.alphasystem.arabic.model.ArabicLetterType;
 import com.alphasystem.morphologicalanalysis.morphology.model.DictionaryNotes;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,18 +22,27 @@ public class DefaultDictionaryNotesRepository implements DictionaryNotesReposito
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
-    private static Query getByFileNameQuery(DictionaryNotes dictionaryNotes) {
-        return new Query(Criteria.where("filename").is(dictionaryNotes.getFileName()));
+    private static Query getQuery(DictionaryNotes dictionaryNotes) {
+        return new Query(Criteria.where("metadata.rootLetters").is(dictionaryNotes.getRootLetters().getDisplayName()));
     }
 
     @Override
     public DictionaryNotes store(DictionaryNotes dictionaryNotes) {
-        GridFSDBFile gridFSFile = gridFsTemplate.findOne(getByFileNameQuery(dictionaryNotes));
+        GridFSDBFile gridFSFile = gridFsTemplate.findOne(getQuery(dictionaryNotes));
         if (gridFSFile != null) {
             // file already exists, delete first
             delete(dictionaryNotes);
         }
-        final GridFSFile file = gridFsTemplate.store(dictionaryNotes.getInputStream(), dictionaryNotes.getFileName(), "plain/text");
+        DBObject metaData = new BasicDBObject();
+        final RootLetters rootLetters = dictionaryNotes.getRootLetters();
+        metaData.put("rootLetters", rootLetters.getDisplayName());
+        metaData.put("firstRadical", rootLetters.getFirstRadical().name());
+        metaData.put("secondRadical", rootLetters.getSecondRadical().name());
+        metaData.put("thirdRadical", rootLetters.getThirdRadical().name());
+        final ArabicLetterType fourthRadical = rootLetters.getFourthRadical();
+        metaData.put("fourthRadical", (fourthRadical == null) ? null : fourthRadical.name());
+        final GridFSFile file = gridFsTemplate.store(dictionaryNotes.getInputStream(), dictionaryNotes.getFileName(),
+                "plain/text", metaData);
         dictionaryNotes.setId(file.getId().toString());
         return dictionaryNotes;
     }
@@ -38,19 +50,20 @@ public class DefaultDictionaryNotesRepository implements DictionaryNotesReposito
     @Override
     public DictionaryNotes retrieve(RootLetters rootLetters) {
         DictionaryNotes dictionaryNotes = new DictionaryNotes(rootLetters);
-        final GridFSDBFile file = gridFsTemplate.findOne(getByFileNameQuery(dictionaryNotes));
+        final GridFSDBFile file = gridFsTemplate.findOne(getQuery(dictionaryNotes));
         if (file == null) {
             // no dictionary notes for these root letters
             return dictionaryNotes;
         }
 
         dictionaryNotes.setId(file.getId().toString());
+        dictionaryNotes.setFileName(file.getFilename());
         dictionaryNotes.setInputStream(file.getInputStream());
         return dictionaryNotes;
     }
 
     @Override
     public void delete(DictionaryNotes dictionaryNotes) {
-        gridFsTemplate.delete(getByFileNameQuery(dictionaryNotes));
+        gridFsTemplate.delete(getQuery(dictionaryNotes));
     }
 }
