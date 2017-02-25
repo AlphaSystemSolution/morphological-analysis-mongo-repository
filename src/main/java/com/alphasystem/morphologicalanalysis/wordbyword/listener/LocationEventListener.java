7ff26e62a00c456fb452537fb7c8e48c3a3cd4be
sfali23 <f.syed.ali@gmail.com>
2017-02-24 20:33:56 -0500

    clean up

--- src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/listener/LocationEventListener.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/listener/LocationEventListener.java
@@ -1,11 +1,9 @@
 package com.alphasystem.morphologicalanalysis.wordbyword.listener;
 
-import com.alphasystem.arabic.model.ArabicWord;
 import com.alphasystem.morphologicalanalysis.morphology.model.MorphologicalEntry;
 import com.alphasystem.morphologicalanalysis.util.MorphologicalAnalysisRepositoryUtil;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
 import com.alphasystem.persistence.mongo.repository.DocumentEventListener;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 import org.springframework.stereotype.Component;
@@ -23,29 +21,11 @@ public class LocationEventListener extends DocumentEventListener<Location> {
     public void onBeforeConvert(BeforeConvertEvent<Location> event) {
         super.onBeforeConvert(event);
         Location source = event.getSource();
-        String text = source.getText();
-        if (StringUtils.isBlank(text)) {
-            ArabicWord locationWord = null;
-            String errorMessage = null;
-            try {
-                locationWord = repositoryUtil.getLocationWord(source);
-            } catch (Exception e) {
-                errorMessage = e.getMessage();
-            }
-            if (locationWord == null) {
-                logger.debug("Error retrieving location text for location \"{}\", error message was \"{}\"", source.getDisplayName(), errorMessage);
-            } else {
-                logger.info("Setting text for location", locationWord.toUnicode());
-                source.setText(locationWord.toUnicode());
-            }
-        }
         MorphologicalEntry morphologicalEntry = source.getMorphologicalEntry();
         if (morphologicalEntry != null) {
             if (morphologicalEntry.isEmpty()) {
                 logger.debug("Trying to save location \"{}\" with empty morphologicalEntry \"{}\"", source, morphologicalEntry);
                 source.setMorphologicalEntry(null);
-            } else {
-                morphologicalEntry.getLocations().add(source);
             }
         }
     }
